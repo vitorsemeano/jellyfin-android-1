Spacetech/jellyfin-android

Implement login UI and logic

app/src/main/java/org/jellyfin/mobile/ui/Common.kt
@@ -1,0 +1,19 @@
+package org.jellyfin.mobile.ui
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+inline fun CenterRow(
+    children: @Composable RowScope.() -> Unit
+) = Row(
+    modifier = Modifier.fillMaxWidth(),
+    horizontalArrangement = Arrangement.Center,
+    verticalGravity = Alignment.CenterVertically,
+    children = children,
+)

vitorsemeano/jellyfin-android-1

app/src/main/java/org/jellyfin/mobile/ui/Common.kt
@@ -1,0 +1,19 @@
+package org.jellyfin.mobile.ui
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+inline fun CenterRow(
+    children: @Composable RowScope.() -> Unit
+) = Row(
+    modifier = Modifier.fillMaxWidth(),
+    horizontalArrangement = Arrangement.Center,
+    verticalAlignment = Alignment.CenterVertically,
+    children = children,
+)