Spacetech/jellyfin-android

Implement music library UI

app/src/main/java/org/jellyfin/mobile/ui/screen/library/music/AlbumList.kt
@@ -1,0 +1,76 @@
+package org.jellyfin.mobile.ui.screen.library.music
+
+import androidx.compose.foundation.Text
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.InnerPadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Stable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.WithConstraints
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.vector.VectorPainter
+import androidx.compose.ui.platform.DensityAmbient
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import org.jellyfin.apiclient.model.entities.ImageType
+import org.jellyfin.mobile.R
+import org.jellyfin.mobile.model.dto.AlbumInfo
+import org.jellyfin.mobile.ui.DefaultCornerRounding
+import org.jellyfin.mobile.ui.utils.ApiImage
+import org.jellyfin.mobile.ui.utils.GridListFor
+import timber.log.Timber
+
+@Composable
+fun AlbumList(viewModel: MusicViewModel) {
+    GridListFor(
+        items = viewModel.albums,
+        numberOfColumns = 3,
+        contentPadding = InnerPadding(top = 8.dp, bottom = 8.dp),
+    ) { album ->
+        Album(albumInfo = album, modifier = Modifier.fillItemMaxWidth(), onClick = {
+            Timber.d("Clicked ${album.name}")
+        })
+    }
+}
+
+@Stable
+@Composable
+fun Album(albumInfo: AlbumInfo, modifier: Modifier = Modifier, onClick: () -> Unit = {}) {
+    Column(
+        modifier = modifier
+            .clip(DefaultCornerRounding)
+            .clickable(onClick = onClick)
+            .padding(8.dp),
+    ) {
+        WithConstraints {
+            val imageSize = with(DensityAmbient.current) { constraints.maxWidth.toDp() }
+            ApiImage(
+                id = albumInfo.id,
+                modifier = Modifier.size(imageSize).clip(DefaultCornerRounding),
+                imageType = ImageType.Primary,
+                imageTags = albumInfo.imageTags,
+                fallback = {
+                    VectorPainter(asset = vectorResource(R.drawable.fallback_image_album_cover))
+                },
+            )
+        }
+        Text(
+            text = albumInfo.name,
+            modifier = Modifier.padding(vertical = 4.dp),
+            overflow = TextOverflow.Ellipsis,
+            maxLines = 1,
+        )
+        Text(
+            text = albumInfo.artist,
+            modifier = Modifier.padding(bottom = 8.dp),
+            overflow = TextOverflow.Ellipsis,
+            maxLines = 1,
+            style = MaterialTheme.typography.caption,
+        )
+    }
+}

app/src/main/java/org/jellyfin/mobile/ui/screen/library/music/ArtistList.kt
@@ -1,0 +1,68 @@
+package org.jellyfin.mobile.ui.screen.library.music
+
+import androidx.compose.foundation.Text
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.InnerPadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Stable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.WithConstraints
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.vector.VectorPainter
+import androidx.compose.ui.platform.DensityAmbient
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import org.jellyfin.apiclient.model.entities.ImageType
+import org.jellyfin.mobile.R
+import org.jellyfin.mobile.model.dto.ArtistInfo
+import org.jellyfin.mobile.ui.DefaultCornerRounding
+import org.jellyfin.mobile.ui.utils.ApiImage
+import org.jellyfin.mobile.ui.utils.GridListFor
+import timber.log.Timber
+
+@Composable
+fun ArtistList(viewModel: MusicViewModel) {
+    GridListFor(
+        items = viewModel.artists,
+        numberOfColumns = 3,
+        contentPadding = InnerPadding(top = 8.dp, bottom = 8.dp),
+    ) { artist ->
+        Artist(artistInfo = artist, modifier = Modifier.fillItemMaxWidth(), onClick = {
+            Timber.d("Clicked ${artist.name}")
+        })
+    }
+}
+
+@Stable
+@Composable
+fun Artist(artistInfo: ArtistInfo, modifier: Modifier = Modifier, onClick: () -> Unit = {}) {
+    Column(
+        modifier = modifier
+            .clip(DefaultCornerRounding)
+            .clickable(onClick = onClick)
+            .padding(8.dp),
+    ) {
+        WithConstraints {
+            val imageSize = with(DensityAmbient.current) { constraints.maxWidth.toDp() }
+            ApiImage(
+                id = artistInfo.id,
+                modifier = Modifier.size(imageSize).clip(DefaultCornerRounding),
+                imageType = ImageType.Primary,
+                imageTags = artistInfo.imageTags,
+                fallback = {
+                    VectorPainter(asset = vectorResource(R.drawable.fallback_image_person))
+                },
+            )
+        }
+        Text(
+            text = artistInfo.name,
+            modifier = Modifier.padding(top = 4.dp, bottom = 8.dp),
+            overflow = TextOverflow.Ellipsis,
+            maxLines = 1,
+        )
+    }
+}

app/src/main/java/org/jellyfin/mobile/ui/screen/library/music/SongList.kt
@@ -1,0 +1,83 @@
+package org.jellyfin.mobile.ui.screen.library.music
+
+import androidx.compose.foundation.Icon
+import androidx.compose.foundation.Text
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumnFor
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Stable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.vector.VectorPainter
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import org.jellyfin.apiclient.model.entities.ImageType
+import org.jellyfin.mobile.R
+import org.jellyfin.mobile.model.dto.SongInfo
+import org.jellyfin.mobile.ui.DefaultCornerRounding
+import org.jellyfin.mobile.ui.utils.ApiImage
+import timber.log.Timber
+
+@Composable
+fun SongList(viewModel: MusicViewModel) {
+    LazyColumnFor(
+        items = viewModel.songs,
+        contentPadding = InnerPadding(top = 8.dp, bottom = 8.dp),
+    ) { song ->
+        Song(songInfo = song, onClick = {
+            Timber.d("Clicked ${song.name}")
+        })
+    }
+}
+
+@Stable
+@Composable
+fun Song(
+    songInfo: SongInfo,
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit = {},
+    onClickMenu: () -> Unit = {}
+) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .height(72.dp)
+            .clickable(onClick = onClick)
+            .padding(start = 16.dp, end = 4.dp)
+            .padding(vertical = 8.dp),
+        verticalGravity = Alignment.CenterVertically,
+    ) {
+        ApiImage(
+            id = songInfo.albumId,
+            modifier = Modifier.size(56.dp).clip(DefaultCornerRounding),
+            imageType = ImageType.Primary,
+            imageTags = songInfo.imageTags,
+            fallback = {
+                VectorPainter(asset = vectorResource(R.drawable.fallback_image_album_cover))
+            },
+        )
+        Column(modifier = Modifier.weight(1f).padding(horizontal = 16.dp)) {
+            Text(
+                text = songInfo.name,
+                modifier = Modifier.padding(bottom = 4.dp),
+                overflow = TextOverflow.Ellipsis,
+                maxLines = 1,
+            )
+            Text(
+                text = songInfo.artist,
+                modifier = Modifier.padding(bottom = 2.dp),
+                overflow = TextOverflow.Ellipsis,
+                maxLines = 1,
+                style = MaterialTheme.typography.caption,
+            )
+        }
+        IconButton(onClick = onClickMenu) {
+            Icon(vectorResource(R.drawable.ic_overflow_white_24dp))
+        }
+    }
+}

vitorsemeano/jellyfin-android-1

app/src/main/java/org/jellyfin/mobile/ui/screen/library/music/AlbumList.kt
@@ -1,0 +1,76 @@
+package org.jellyfin.mobile.ui.screen.library.music
+
+import androidx.compose.foundation.Text
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Stable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.WithConstraints
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.vector.VectorPainter
+import androidx.compose.ui.platform.DensityAmbient
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import org.jellyfin.apiclient.model.entities.ImageType
+import org.jellyfin.mobile.R
+import org.jellyfin.mobile.model.dto.AlbumInfo
+import org.jellyfin.mobile.ui.DefaultCornerRounding
+import org.jellyfin.mobile.ui.utils.ApiImage
+import org.jellyfin.mobile.ui.utils.GridListFor
+import timber.log.Timber
+
+@Composable
+fun AlbumList(viewModel: MusicViewModel) {
+    GridListFor(
+        items = viewModel.albums,
+        numberOfColumns = 3,
+        contentPadding = PaddingValues(top = 8.dp, bottom = 8.dp),
+    ) { album ->
+        Album(albumInfo = album, modifier = Modifier.fillItemMaxWidth(), onClick = {
+            Timber.d("Clicked ${album.name}")
+        })
+    }
+}
+
+@Stable
+@Composable
+fun Album(albumInfo: AlbumInfo, modifier: Modifier = Modifier, onClick: () -> Unit = {}) {
+    Column(
+        modifier = modifier
+            .clip(DefaultCornerRounding)
+            .clickable(onClick = onClick)
+            .padding(8.dp),
+    ) {
+        WithConstraints {
+            val imageSize = with(DensityAmbient.current) { constraints.maxWidth.toDp() }
+            ApiImage(
+                id = albumInfo.id,
+                modifier = Modifier.size(imageSize).clip(DefaultCornerRounding),
+                imageType = ImageType.Primary,
+                imageTags = albumInfo.imageTags,
+                fallback = {
+                    VectorPainter(asset = vectorResource(R.drawable.fallback_image_album_cover))
+                },
+            )
+        }
+        Text(
+            text = albumInfo.name,
+            modifier = Modifier.padding(vertical = 4.dp),
+            overflow = TextOverflow.Ellipsis,
+            maxLines = 1,
+        )
+        Text(
+            text = albumInfo.artist,
+            modifier = Modifier.padding(bottom = 8.dp),
+            overflow = TextOverflow.Ellipsis,
+            maxLines = 1,
+            style = MaterialTheme.typography.caption,
+        )
+    }
+}

app/src/main/java/org/jellyfin/mobile/ui/screen/library/music/ArtistList.kt
@@ -1,0 +1,68 @@
+package org.jellyfin.mobile.ui.screen.library.music
+
+import androidx.compose.foundation.Text
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Stable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.WithConstraints
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.vector.VectorPainter
+import androidx.compose.ui.platform.DensityAmbient
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import org.jellyfin.apiclient.model.entities.ImageType
+import org.jellyfin.mobile.R
+import org.jellyfin.mobile.model.dto.ArtistInfo
+import org.jellyfin.mobile.ui.DefaultCornerRounding
+import org.jellyfin.mobile.ui.utils.ApiImage
+import org.jellyfin.mobile.ui.utils.GridListFor
+import timber.log.Timber
+
+@Composable
+fun ArtistList(viewModel: MusicViewModel) {
+    GridListFor(
+        items = viewModel.artists,
+        numberOfColumns = 3,
+        contentPadding = PaddingValues(top = 8.dp, bottom = 8.dp),
+    ) { artist ->
+        Artist(artistInfo = artist, modifier = Modifier.fillItemMaxWidth(), onClick = {
+            Timber.d("Clicked ${artist.name}")
+        })
+    }
+}
+
+@Stable
+@Composable
+fun Artist(artistInfo: ArtistInfo, modifier: Modifier = Modifier, onClick: () -> Unit = {}) {
+    Column(
+        modifier = modifier
+            .clip(DefaultCornerRounding)
+            .clickable(onClick = onClick)
+            .padding(8.dp),
+    ) {
+        WithConstraints {
+            val imageSize = with(DensityAmbient.current) { constraints.maxWidth.toDp() }
+            ApiImage(
+                id = artistInfo.id,
+                modifier = Modifier.size(imageSize).clip(DefaultCornerRounding),
+                imageType = ImageType.Primary,
+                imageTags = artistInfo.imageTags,
+                fallback = {
+                    VectorPainter(asset = vectorResource(R.drawable.fallback_image_person))
+                },
+            )
+        }
+        Text(
+            text = artistInfo.name,
+            modifier = Modifier.padding(top = 4.dp, bottom = 8.dp),
+            overflow = TextOverflow.Ellipsis,
+            maxLines = 1,
+        )
+    }
+}

app/src/main/java/org/jellyfin/mobile/ui/screen/library/music/SongList.kt
@@ -1,0 +1,83 @@
+package org.jellyfin.mobile.ui.screen.library.music
+
+import androidx.compose.foundation.Icon
+import androidx.compose.foundation.Text
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumnFor
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Stable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.vector.VectorPainter
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import org.jellyfin.apiclient.model.entities.ImageType
+import org.jellyfin.mobile.R
+import org.jellyfin.mobile.model.dto.SongInfo
+import org.jellyfin.mobile.ui.DefaultCornerRounding
+import org.jellyfin.mobile.ui.utils.ApiImage
+import timber.log.Timber
+
+@Composable
+fun SongList(viewModel: MusicViewModel) {
+    LazyColumnFor(
+        items = viewModel.songs,
+        contentPadding = PaddingValues(top = 8.dp, bottom = 8.dp),
+    ) { song ->
+        Song(songInfo = song, onClick = {
+            Timber.d("Clicked ${song.name}")
+        })
+    }
+}
+
+@Stable
+@Composable
+fun Song(
+    songInfo: SongInfo,
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit = {},
+    onClickMenu: () -> Unit = {}
+) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .height(72.dp)
+            .clickable(onClick = onClick)
+            .padding(start = 16.dp, end = 4.dp)
+            .padding(vertical = 8.dp),
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        ApiImage(
+            id = songInfo.albumId,
+            modifier = Modifier.size(56.dp).clip(DefaultCornerRounding),
+            imageType = ImageType.Primary,
+            imageTags = songInfo.imageTags,
+            fallback = {
+                VectorPainter(asset = vectorResource(R.drawable.fallback_image_album_cover))
+            },
+        )
+        Column(modifier = Modifier.weight(1f).padding(horizontal = 16.dp)) {
+            Text(
+                text = songInfo.name,
+                modifier = Modifier.padding(bottom = 4.dp),
+                overflow = TextOverflow.Ellipsis,
+                maxLines = 1,
+            )
+            Text(
+                text = songInfo.artist,
+                modifier = Modifier.padding(bottom = 2.dp),
+                overflow = TextOverflow.Ellipsis,
+                maxLines = 1,
+                style = MaterialTheme.typography.caption,
+            )
+        }
+        IconButton(onClick = onClickMenu) {
+            Icon(vectorResource(R.drawable.ic_overflow_white_24dp))
+        }
+    }
+}