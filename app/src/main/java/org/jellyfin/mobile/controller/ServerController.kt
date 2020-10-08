Spacetech/jellyfin-android

Implement home screen with user views

app/src/main/java/org/jellyfin/mobile/controller/ServerController.kt
@@ -11,14 +11,17 @@ import okhttp3.HttpUrl
 import okhttp3.HttpUrl.Companion.toHttpUrlOrNull
 import org.jellyfin.apiclient.Jellyfin
 import org.jellyfin.apiclient.interaction.ApiClient
+import org.jellyfin.apiclient.model.dto.UserDto
 import org.jellyfin.apiclient.model.system.PublicSystemInfo
 import org.jellyfin.mobile.R
+import org.jellyfin.mobile.model.dto.UserInfo
 import org.jellyfin.mobile.model.sql.dao.ServerDao
 import org.jellyfin.mobile.model.state.CheckUrlState
 import org.jellyfin.mobile.model.state.LoginState
 import org.jellyfin.mobile.utils.PRODUCT_NAME_SUPPORTED_SINCE
 import org.jellyfin.mobile.utils.authenticateUser
 import org.jellyfin.mobile.utils.getPublicSystemInfo
+import org.jellyfin.mobile.utils.getUserInfo
 
 class ServerController(
     private val jellyfin: Jellyfin,
@@ -28,6 +31,7 @@ class ServerController(
     private val scope = CoroutineScope(Dispatchers.Default)
 
     var loginState by mutableStateOf(LoginState.PENDING)
+    var userInfo by mutableStateOf<UserInfo?>(null)
 
     init {
         scope.launch {
@@ -37,6 +41,7 @@ class ServerController(
             loginState = if (serverInfo != null) {
                 apiClient.ChangeServerLocation(serverInfo.hostname)
                 apiClient.SetAuthenticationInfo(serverInfo.accessToken, serverInfo.userId)
+                userInfo = apiClient.getUserInfo(serverInfo.userId)?.toUserInfo()
                 LoginState.LOGGED_IN
             } else {
                 LoginState.NOT_LOGGED_IN
@@ -84,12 +89,29 @@ class ServerController(
         requireNotNull(apiClient.serverAddress) { "Server address not set" }
         val authResult = apiClient.authenticateUser(username, password)
         if (authResult != null) {
+            val user = authResult.user
             withContext(Dispatchers.IO) {
-                serverDao.insert(apiClient.serverAddress, authResult.user.id, authResult.accessToken)
+                serverDao.insert(apiClient.serverAddress, user.id, authResult.accessToken)
             }
+            userInfo = user?.toUserInfo()
             loginState = LoginState.LOGGED_IN
             return true
         }
         return false
     }
+
+    fun tryLogout() {
+        scope.launch { logout() }
+    }
+
+    suspend fun logout() {
+        withContext(Dispatchers.IO) {
+            serverDao.logout(apiClient.serverAddress)
+        }
+        apiClient.ChangeServerLocation(null)
+        loginState = LoginState.NOT_LOGGED_IN
+        userInfo = null
+    }
 }
+
+private fun UserDto.toUserInfo() = UserInfo(id, name, primaryImageTag)

app/src/main/java/org/jellyfin/mobile/ui/Common.kt
@@ -1,12 +1,46 @@
 package org.jellyfin.mobile.ui
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.RowScope
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedButton
+import androidx.compose.material.Scaffold
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.unit.dp
+
+val DefaultCornerRounding = RoundedCornerShape(8.dp)
+
+@Composable
+fun ScreenScaffold(
+    modifier: Modifier = Modifier,
+    title: String,
+    titleFont: FontFamily? = null,
+    actions: @Composable RowScope.() -> Unit,
+    content: @Composable (InnerPadding) -> Unit
+) {
+    Scaffold(
+        modifier = modifier,
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = title,
+                        fontFamily = titleFont,
+                    )
+                },
+                actions = actions,
+                backgroundColor = MaterialTheme.colors.primary,
+            )
+        },
+        bodyContent = content,
+    )
+}
 
 @Composable
 inline fun CenterRow(
@@ -17,3 +51,15 @@ inline fun CenterRow(
     verticalGravity = Alignment.CenterVertically,
     children = children,
 )
+
+@Composable
+fun ChipletButton(
+    text: String,
+    onClick: () -> Unit
+) = OutlinedButton(
+    onClick = onClick,
+    modifier = Modifier.padding(8.dp),
+    shape = CircleShape,
+) {
+    Text(text = text, color = MaterialTheme.colors.onSurface)
+}

app/src/main/java/org/jellyfin/mobile/ui/screen/home/UserDetails.kt
@@ -1,0 +1,98 @@
+package org.jellyfin.mobile.ui.screen.home
+
+import androidx.compose.foundation.InteractionState
+import androidx.compose.foundation.Text
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.ContextAmbient
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Popup
+import org.jellyfin.mobile.R
+import org.jellyfin.mobile.controller.ServerController
+import org.jellyfin.mobile.model.dto.UserInfo
+import org.jellyfin.mobile.ui.ChipletButton
+import org.jellyfin.mobile.ui.DefaultCornerRounding
+import org.jellyfin.mobile.ui.utils.ApiUserImage
+import org.jellyfin.mobile.utils.toast
+
+@Composable
+fun UserImage(modifier: Modifier = Modifier, user: UserInfo) {
+    Surface(
+        modifier = Modifier.size(56.dp).padding(8.dp).clip(CircleShape).then(modifier),
+        color = MaterialTheme.colors.primaryVariant,
+    ) {
+        ApiUserImage(
+            userInfo = user,
+            modifier = Modifier.size(40.dp),
+        )
+    }
+}
+
+@Composable
+fun UserDetailsButton(
+    modifier: Modifier = Modifier,
+    user: UserInfo,
+    showUserDetails: (Boolean) -> Unit
+) {
+    UserImage(
+        modifier = modifier.clickable(interactionState = remember { InteractionState() }) {
+            showUserDetails(true)
+        },
+        user = user,
+    )
+}
+
+@Composable
+fun UserDetails(
+    serverController: ServerController,
+    user: UserInfo,
+    showUserDetails: (Boolean) -> Unit
+) {
+    Popup(
+        alignment = Alignment.TopCenter,
+        isFocusable = true,
+        onDismissRequest = {
+            showUserDetails(false)
+        },
+    ) {
+        Surface(
+            modifier = Modifier.fillMaxWidth().padding(top = 8.dp),
+            shape = DefaultCornerRounding,
+        ) {
+            Column(modifier = Modifier.padding(8.dp).fillMaxWidth()) {
+                Row(verticalGravity = Alignment.CenterVertically) {
+                    UserImage(user = user)
+                    Text(
+                        text = user.name,
+                        modifier = Modifier.padding(horizontal = 8.dp),
+                    )
+                }
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.End,
+                ) {
+                    val context = ContextAmbient.current
+                    ChipletButton(
+                        text = stringResource(R.string.profile_button_text),
+                        onClick = {
+                            context.toast("Not implemented")
+                        },
+                    )
+                    ChipletButton(
+                        text = stringResource(R.string.logout_button_text),
+                        onClick = serverController::tryLogout,
+                    )
+                }
+            }
+        }
+    }
+}

app/src/main/java/org/jellyfin/mobile/ui/screen/home/UserViews.kt
@@ -1,0 +1,44 @@
+package org.jellyfin.mobile.ui.screen.home
+
+import androidx.compose.foundation.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRowFor
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
+import org.jellyfin.apiclient.model.entities.ImageType
+import org.jellyfin.mobile.controller.LibraryController
+import org.jellyfin.mobile.model.dto.UserViewInfo
+import org.jellyfin.mobile.ui.DefaultCornerRounding
+import org.jellyfin.mobile.ui.utils.ApiImage
+
+@Composable
+fun UserViews(libraryController: LibraryController) {
+    LazyRowFor(
+        items = libraryController.userViews,
+        modifier = Modifier.fillMaxWidth(1f),
+        contentPadding = InnerPadding(start = 12.dp, end = 12.dp),
+    ) { item ->
+        UserView(item = item)
+    }
+}
+
+@Composable
+fun UserView(item: UserViewInfo) {
+    Column(modifier = Modifier.padding(4.dp)) {
+        val width = 256.dp
+        val height = 144.dp
+        ApiImage(
+            id = item.id,
+            modifier = Modifier.width(width).height(height).clip(DefaultCornerRounding),
+            imageType = ImageType.Primary,
+            imageTags = item.imageTags,
+        )
+        Text(
+            text = item.name,
+            modifier = Modifier.gravity(Alignment.CenterHorizontally).padding(vertical = 8.dp),
+        )
+    }
+}

vitorsemeano/jellyfin-android-1

app/src/main/java/org/jellyfin/mobile/controller/ServerController.kt
@@ -11,9 +11,11 @@ import okhttp3.HttpUrl
 import okhttp3.HttpUrl.Companion.toHttpUrlOrNull
 import org.jellyfin.apiclient.Jellyfin
 import org.jellyfin.apiclient.interaction.ApiClient
+import org.jellyfin.apiclient.model.dto.UserDto
 import org.jellyfin.apiclient.model.system.PublicSystemInfo
 import org.jellyfin.mobile.AppPreferences
 import org.jellyfin.mobile.R
+import org.jellyfin.mobile.model.dto.UserInfo
 import org.jellyfin.mobile.model.sql.dao.ServerDao
 import org.jellyfin.mobile.model.sql.dao.UserDao
 import org.jellyfin.mobile.model.state.CheckUrlState
@@ -21,6 +23,7 @@ import org.jellyfin.mobile.model.state.LoginState
 import org.jellyfin.mobile.utils.PRODUCT_NAME_SUPPORTED_SINCE
 import org.jellyfin.mobile.utils.authenticateUser
 import org.jellyfin.mobile.utils.getPublicSystemInfo
+import org.jellyfin.mobile.utils.getUserInfo
 
 class ServerController(
     private val jellyfin: Jellyfin,
@@ -32,6 +35,7 @@ class ServerController(
     private val scope = CoroutineScope(Dispatchers.Main)
 
     var loginState by mutableStateOf(LoginState.PENDING)
+    var userInfo by mutableStateOf<UserInfo?>(null)
 
     init {
         scope.launch {
@@ -44,6 +48,7 @@ class ServerController(
                 val (user, server) = serverUser
                 apiClient.ChangeServerLocation(server.hostname)
                 apiClient.SetAuthenticationInfo(user.accessToken, user.id)
+                userInfo = apiClient.getUserInfo(user.id)?.toUserInfo()
                 LoginState.LOGGED_IN
             } else {
                 LoginState.NOT_LOGGED_IN
@@ -91,13 +96,32 @@ class ServerController(
         requireNotNull(apiClient.serverAddress) { "Server address not set" }
         val authResult = apiClient.authenticateUser(username, password)
         if (authResult != null) {
+            val user = authResult.user
             withContext(Dispatchers.IO) {
                 val serverId = serverDao.insert(apiClient.serverAddress)
-                userDao.insert(serverId, authResult.user.id, authResult.accessToken)
+                userDao.insert(serverId, user.id, authResult.accessToken)
             }
+            userInfo = user?.toUserInfo()
             loginState = LoginState.LOGGED_IN
             return true
         }
         return false
     }
+
+    fun tryLogout() {
+        scope.launch { logout() }
+    }
+
+    suspend fun logout() {
+        userInfo?.let { user ->
+            withContext(Dispatchers.IO) {
+                userDao.logout(user.id)
+            }
+        }
+        apiClient.ChangeServerLocation(null)
+        loginState = LoginState.NOT_LOGGED_IN
+        userInfo = null
+    }
 }
+
+private fun UserDto.toUserInfo() = UserInfo(id, name, primaryImageTag)

app/src/main/java/org/jellyfin/mobile/ui/Common.kt
@@ -1,12 +1,46 @@
 package org.jellyfin.mobile.ui
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.RowScope
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedButton
+import androidx.compose.material.Scaffold
+import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.unit.dp
+
+val DefaultCornerRounding = RoundedCornerShape(8.dp)
+
+@Composable
+fun ScreenScaffold(
+    modifier: Modifier = Modifier,
+    title: String,
+    titleFont: FontFamily? = null,
+    actions: @Composable RowScope.() -> Unit,
+    content: @Composable (PaddingValues) -> Unit
+) {
+    Scaffold(
+        modifier = modifier,
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = title,
+                        fontFamily = titleFont,
+                    )
+                },
+                actions = actions,
+                backgroundColor = MaterialTheme.colors.primary,
+            )
+        },
+        bodyContent = content,
+    )
+}
 
 @Composable
 inline fun CenterRow(
@@ -17,3 +51,15 @@ inline fun CenterRow(
     verticalAlignment = Alignment.CenterVertically,
     children = children,
 )
+
+@Composable
+fun ChipletButton(
+    text: String,
+    onClick: () -> Unit
+) = OutlinedButton(
+    onClick = onClick,
+    modifier = Modifier.padding(8.dp),
+    shape = CircleShape,
+) {
+    Text(text = text, color = MaterialTheme.colors.onSurface)
+}

app/src/main/java/org/jellyfin/mobile/ui/screen/home/UserDetails.kt
@@ -1,0 +1,98 @@
+package org.jellyfin.mobile.ui.screen.home
+
+import androidx.compose.foundation.InteractionState
+import androidx.compose.foundation.Text
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.ContextAmbient
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Popup
+import org.jellyfin.mobile.R
+import org.jellyfin.mobile.controller.ServerController
+import org.jellyfin.mobile.model.dto.UserInfo
+import org.jellyfin.mobile.ui.ChipletButton
+import org.jellyfin.mobile.ui.DefaultCornerRounding
+import org.jellyfin.mobile.ui.utils.ApiUserImage
+import org.jellyfin.mobile.utils.toast
+
+@Composable
+fun UserImage(modifier: Modifier = Modifier, user: UserInfo) {
+    Surface(
+        modifier = Modifier.size(56.dp).padding(8.dp).clip(CircleShape).then(modifier),
+        color = MaterialTheme.colors.primaryVariant,
+    ) {
+        ApiUserImage(
+            userInfo = user,
+            modifier = Modifier.size(40.dp),
+        )
+    }
+}
+
+@Composable
+fun UserDetailsButton(
+    modifier: Modifier = Modifier,
+    user: UserInfo,
+    showUserDetails: (Boolean) -> Unit
+) {
+    UserImage(
+        modifier = modifier.clickable(interactionState = remember { InteractionState() }) {
+            showUserDetails(true)
+        },
+        user = user,
+    )
+}
+
+@Composable
+fun UserDetails(
+    serverController: ServerController,
+    user: UserInfo,
+    showUserDetails: (Boolean) -> Unit
+) {
+    Popup(
+        alignment = Alignment.TopCenter,
+        isFocusable = true,
+        onDismissRequest = {
+            showUserDetails(false)
+        },
+    ) {
+        Surface(
+            modifier = Modifier.fillMaxWidth().padding(top = 8.dp),
+            shape = DefaultCornerRounding,
+        ) {
+            Column(modifier = Modifier.padding(8.dp).fillMaxWidth()) {
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    UserImage(user = user)
+                    Text(
+                        text = user.name,
+                        modifier = Modifier.padding(horizontal = 8.dp),
+                    )
+                }
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.End,
+                ) {
+                    val context = ContextAmbient.current
+                    ChipletButton(
+                        text = stringResource(R.string.profile_button_text),
+                        onClick = {
+                            context.toast("Not implemented")
+                        },
+                    )
+                    ChipletButton(
+                        text = stringResource(R.string.logout_button_text),
+                        onClick = serverController::tryLogout,
+                    )
+                }
+            }
+        }
+    }
+}

app/src/main/java/org/jellyfin/mobile/ui/screen/home/UserViews.kt
@@ -1,0 +1,44 @@
+package org.jellyfin.mobile.ui.screen.home
+
+import androidx.compose.foundation.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRowFor
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
+import org.jellyfin.apiclient.model.entities.ImageType
+import org.jellyfin.mobile.controller.LibraryController
+import org.jellyfin.mobile.model.dto.UserViewInfo
+import org.jellyfin.mobile.ui.DefaultCornerRounding
+import org.jellyfin.mobile.ui.utils.ApiImage
+
+@Composable
+fun UserViews(libraryController: LibraryController) {
+    LazyRowFor(
+        items = libraryController.userViews,
+        modifier = Modifier.fillMaxWidth(1f),
+        contentPadding = PaddingValues(start = 12.dp, end = 12.dp),
+    ) { item ->
+        UserView(item = item)
+    }
+}
+
+@Composable
+fun UserView(item: UserViewInfo) {
+    Column(modifier = Modifier.padding(4.dp)) {
+        val width = 256.dp
+        val height = 144.dp
+        ApiImage(
+            id = item.id,
+            modifier = Modifier.width(width).height(height).clip(DefaultCornerRounding),
+            imageType = ImageType.Primary,
+            imageTags = item.imageTags,
+        )
+        Text(
+            text = item.name,
+            modifier = Modifier.gravity(Alignment.CenterHorizontally).padding(vertical = 8.dp),
+        )
+    }
+}